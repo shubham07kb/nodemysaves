337eb1de05c11d1e2dd6e4a833a5f71e1b497189
Shubham Kumar Bansal <kumarbansal.shubham07@gmail.com>
2022-12-16 21:26:07 +0530

    ready to develop site

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,11 @@
 const ip = require('./ip');
 const prepage = require('./pages/prepage');
+const blogpage = require('./pages/blog');
+const adminpage = require('./pages/admin');
+const appstorepage = require('./pages/appstore');
+const apppage = require('./pages/app');
+const collectionpage = require('./pages/collection');
+const projectpage = require('./pages/project');
 const short = require('./shortlink');
 const useragent = require('./useragent');
 const proxylink = require('./proxylink');
@@ -7,7 +13,19 @@ module.exports = {
     userpraser: useragent.uapraser,
     getip: ip.getip,
     prepagehtml: prepage.html,
+    blogpagehtml: blogpage.html,
+    adminpagehtml: adminpage.html,
+    appstorepagehtml: appstorepage.html,
+    apppagehtml: apppage.html,
+    collectionpagehtml: collectionpage.html,
+    projectpagehtml: projectpage.html,
     prepagemeta: prepage.meta,
+    blogpagemeta: blogpage.meta,
+    adminpagemeta: adminpage.meta,
+    appstorepagemeta: appstorepage.meta,
+    apppagemeta: apppage.meta,
+    collectionpagemeta: collectionpage.meta,
+    projectpagemeta: projectpage.meta,
     shortlink: short.direct,
     shortlinkapi: short.api,
     proxylink: proxylink.direct
---
+++ server/api/pages/project/index.js
@@ -0,0 +1,10 @@
+function meta(page,req,res){
+    
+}
+function html(page,req,res){
+    
+}
+module.exports={
+    meta:meta,
+    html:html
+}
